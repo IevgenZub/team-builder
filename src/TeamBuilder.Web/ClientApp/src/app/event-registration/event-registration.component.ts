07081d26907affcc6af31fee14bc7f1de026a7ee
Ievgen-Zubkov <ievgen_zubkov@epam.com>
2019-09-11 14:54:44 +0300

    Add ag-grid options

--- src/TeamBuilder.Web/ClientApp/src/app/event-registration/event-registration.component.ts
+++ src/TeamBuilder.Web/ClientApp/src/app/event-registration/event-registration.component.ts
@@ -1,24 +1,67 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { GridOptions } from "ag-grid-community";
 
 @Component({
   selector: 'event-registration-component',
   templateUrl: './event-registration.component.html'
 })
 export class EventRegistrationComponent {
-  private eventForm;
+  private eventForm: FormGroup;
+  public gridOptions: GridOptions;
   public teamEvents: any;
-  public columnDefs = [
-    { headerName: 'Name', field: 'name', resizable: true },
-    { headerName: 'Location', field: 'location', resizable: true }
-  ];
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string, formBuilder: FormBuilder) {
     this.eventForm = formBuilder.group({
       name: '',
       location: ''
     });
+
+    this.gridOptions = <GridOptions>{
+      enableRangeSelection: true,
+      columnDefs: this.createColumnDefs(),
+
+      deltaRowDataMode: true,
+      getRowNodeId: function (data) {
+        // the code is unique, so perfect for the id
+        return data.id;
+      },
+
+      onGridReady: () => {
+        this.teamEvents.subscribe(
+          rowData => {
+            // the initial full set of data
+            // note that we don't need to un-subscribe here as it's a one off data load
+            if (this.gridOptions.api) { // can be null when tabbing between the examples
+              this.gridOptions.api.setRowData(rowData);
+            }
+          }
+        );
+      },
+
+      onFirstDataRendered(params) {
+        params.api.sizeColumnsToFit();
+      }
+    };
+  }
+
+  private createColumnDefs() {
+    return [
+      { headerName: "Name", field: "name", width: 280, resizable: true },
+      {
+        headerName: "Location", field: "location", width: 100, resizable: true,
+        cellRenderer: 'agAnimateSlideCellRenderer'
+      }
+    ]
+  }
+
+  numberFormatter(params) {
+    if (typeof params.value === 'number') {
+      return params.value.toFixed(2);
+    } else {
+      return params.value;
+    }
   }
 
   ngOnInit() {
@@ -27,7 +70,11 @@ export class EventRegistrationComponent {
 
   onSubmit(eventData) {
     this.http.post<EventRegistration>(this.baseUrl + 'api/teamevents', eventData).subscribe(
-      //result => this.teamEvents.push(result),
+      result => {
+        if (this.gridOptions.api) {
+          this.gridOptions.api.updateRowData({ add: [result]})
+        }
+      },
       error => console.error(error)
     );
 
