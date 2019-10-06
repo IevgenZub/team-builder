8561015213a43f7a69b61d7430dcb48d8a390c17
Ievgen-Zubkov <ievgen_zubkov@epam.com>
2019-10-06 17:10:27 +0300

    Use wiki search api

--- src/TeamBuilder.Web/Services/TextAnalyticsService.cs
+++ src/TeamBuilder.Web/Services/TextAnalyticsService.cs
@@ -11,6 +11,7 @@ namespace TeamBuilder.Web.Services
     {
         private const string AzureTextAnalyticsKey = "ced608eaa18b4bd195cd895dce0ec44c";
         private const string AzureTextAnalyticsEndpoint = "https://team-builder-text-analytics.cognitiveservices.azure.com/";
+        private const string WikiSearchEndpoint = "https://en.wikipedia.org/w/api.php";
         
         private readonly TextAnalyticsClient _client = new TextAnalyticsClient(
             new ApiKeyServiceClientCredentials(AzureTextAnalyticsKey)) { Endpoint = AzureTextAnalyticsEndpoint };
@@ -18,20 +19,28 @@ namespace TeamBuilder.Web.Services
         public async Task<string> BuildTextWithLinksAsync(string input)
         {
             var result = await _client.EntitiesAsync(input, "en");
+            string wikiResponseRaw = null;
             foreach (var entity in result.Entities)
             {
-                var httpClient = new HttpClient();
-                var wikiResponseRaw = await httpClient.GetStringAsync("https://en.wikipedia.org/w/api.php?action=opensearch&"
-                    + $"search={HttpUtility.UrlEncode(entity.Name)}&limit=1&namespace=0&format=json");
-                
-                var wikiResponse = (JArray)JsonConvert.DeserializeObject(wikiResponseRaw);
-                if (wikiResponse.Count == 4)
+                using (var httpClient = new HttpClient())
                 {
-                    var links = (JArray)wikiResponse[3];
-                    if (links.Count == 1)
+                    var encodedEntity = HttpUtility.UrlEncode(entity.Name);
+
+                    wikiResponseRaw = await httpClient.GetStringAsync(
+                        $"{WikiSearchEndpoint}?action=opensearch&search={encodedEntity}&limit=1&namespace=0&format=json");
+                }
+
+                if (!string.IsNullOrEmpty(wikiResponseRaw))
+                {
+                    var wikiResponse = (JArray)JsonConvert.DeserializeObject(wikiResponseRaw);
+                    if (wikiResponse.Count == 4)
                     {
-                        var link = ((JArray)wikiResponse[3])[0].Value<string>();
-                        input = input.Replace(entity.Name, $"<a target='_blank' href='{link}'>{entity.Name}</a>");
+                        var links = (JArray)wikiResponse[3];
+                        if (links.Count == 1)
+                        {
+                            var link = ((JArray)wikiResponse[3])[0].Value<string>();
+                            input = input.Replace(entity.Name, $"<a target='_blank' href='{link}'>{entity.Name}</a>");
+                        }
                     }
                 }
             }
